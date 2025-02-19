f0c8e2a99ef127819e017a7be21a5122c2d2c17a
Ahmadreza1972 <azadia2@mcmaster.ca>
2025-02-19 14:36:57 -0500

    meta model

--- models/DataLoad.py
+++ models/DataLoad.py
@@ -14,9 +14,12 @@ class DataLoad:
 
     # Load dataset from .pth file
     def load_data(self,path):
-        raw_data = torch.load(path)
+        raw_data = torch.load(path, weights_only=False)
         data = raw_data['data']
-        labels = raw_data['labels'].tolist()
+        if isinstance(raw_data['labels'], list):
+            labels = raw_data['labels']
+        else:
+            labels = raw_data['labels'].tolist()
         indices = [] #raw_data['indices']
         return data, labels , indices
     
@@ -45,29 +48,42 @@ class DataLoad:
         transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])  # Normalize the images
         ])
         # Convert tensor to PIL Image for transformation
-        images_pil = [transforms.ToPILImage()(img.permute(2, 0, 1)) for img in images]    
+        print(images[0].shape)
+        if images[0].shape[0]==3:
+            images_pil = [transforms.ToPILImage()(img.permute(0,1,2)) for img in images]    
+        else:
+            images_pil = [transforms.ToPILImage()(img.permute(2,0,1)) for img in images]
         images = torch.stack([transform(img) for img in images_pil])  # Apply transformation to each image
         
-            
-        train_size = int((1-self._valdata_ratio) * len(images))
-        val_size = len(images) - train_size
-        train_dataset, val_dataset = random_split(images, [train_size, val_size])
+
+        if self._test_path!=None:
+            train_size = int((1-self._valdata_ratio) * len(images))
+            val_size = len(images) - train_size
+            train_dataset, val_dataset = random_split(images, [train_size, val_size])
         
-        train_images = images[train_dataset.indices]
-        val_images = images[val_dataset.indices]
+            train_images = images[train_dataset.indices]
+            val_images = images[val_dataset.indices]
         
-        train_labels = [labels[i] for i in train_dataset.indices]
-        val_labels = [labels[i] for i in val_dataset.indices]
+            train_labels = [labels[i] for i in train_dataset.indices]
+            val_labels = [labels[i] for i in val_dataset.indices]
         
-        train_loader = self.create_dataloader(train_images, train_labels, batch_size=self._batch_size)
-        val_loader = self.create_dataloader(val_images, val_labels, batch_size=self._batch_size)
+            train_loader = self.create_dataloader(train_images, train_labels, batch_size=self._batch_size)
+            val_loader = self.create_dataloader(val_images, val_labels, batch_size=self._batch_size)
  
-        # Load and transform test data
-        test_images, test_labels, _ = self.load_data(self._test_path)
-        test_images_pil = [transforms.ToPILImage()(img.permute(2, 0, 1)) for img in test_images]
-        test_images = torch.stack([transform(img) for img in test_images_pil])
-        test_loader = self.create_dataloader(test_images, test_labels, batch_size=self._batch_size)
+            # Load and transform test data
+            test_images, test_labels, _ = self.load_data(self._test_path)
             
+            if test_images[0].shape[0]==3:
+                test_images_pil = [transforms.ToPILImage()(img.permute(0,1,2)) for img in test_images]    
+            else:
+                test_images_pil = [transforms.ToPILImage()(img.permute(2,0,1)) for img in test_images]
+
+            test_images = torch.stack([transform(img) for img in test_images_pil])
+            test_loader = self.create_dataloader(test_images, test_labels, batch_size=self._batch_size)
+        else:
+            train_loader = self.create_dataloader(images, labels, batch_size=self._batch_size)
+            val_loader=[]
+            test_loader=[]
         return train_loader,val_loader,test_loader
         
         
